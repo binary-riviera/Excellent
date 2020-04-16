ca5fad7fafa19b320201695def94fd6504c00da2
Daniel Cripps <binary.riviera@gmail.com>
2020-04-16 17:31:00 +0100

    Open test xslx file

--- src/main/java/org/excellent/App.java
+++ src/main/java/org/excellent/App.java
@@ -2,10 +2,12 @@ package org.excellent;
 
 import org.excellent.ExcelReader;
 
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello, World");
         ExcelReader reader = new ExcelReader();
-        reader.readFile("Foo");
+        reader.readFile("static_files/Example.xlsx");
     }
 }
--- src/main/java/org/excellent/ExcelReader.java
+++ src/main/java/org/excellent/ExcelReader.java
@@ -1,8 +1,29 @@
 package org.excellent;
 
+import org.apache.poi.hssf.eventusermodel.HSSFRequest;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
 public class ExcelReader {
 
-    public void readFile(String filename) {
+    public void readFile(String filename) throws IOException {
         System.out.println(filename);
+        File file = new File(filename);
+        FileInputStream fIP = new FileInputStream(file);
+
+        // Get the workbook instance for XLSX file
+        XSSFWorkbook workbook = new XSSFWorkbook(fIP);
+
+        if (file.isFile() && file.exists()) {
+            System.out.println("Excel file opened successfully");
+        } else {
+            System.out.println("Error opening file");
+        }
+
     }
 }
