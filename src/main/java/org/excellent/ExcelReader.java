068912db59da5ccf395dca1895eb88b3ffa82ee6
Daniel Cripps <binary.riviera@gmail.com>
2020-04-17 19:32:57 +0100

    Got it to show cell values, not cell formulas

--- src/main/java/org/excellent/ExcelReader.java
+++ src/main/java/org/excellent/ExcelReader.java
@@ -1,12 +1,15 @@
 package org.excellent;
 
-import org.apache.poi.hssf.eventusermodel.HSSFRequest;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 
 public class ExcelReader {
@@ -25,5 +28,30 @@ public class ExcelReader {
             System.out.println("Error opening file");
         }
 
+        // now we have the workbook open, let's get just the first sheet
+        XSSFSheet spreadsheet = workbook.getSheetAt(0);
+        Iterator<Row> rowIterator = spreadsheet.iterator();
+        XSSFRow row;
+
+        while (rowIterator.hasNext()) {
+            row = (XSSFRow) rowIterator.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+
+                switch (cell.getCellType()) {
+                    case NUMERIC:
+                        System.out.print(cell.getNumericCellValue() + "\t\t");
+                        break;
+
+                    case STRING:
+                        System.out.print(cell.getStringCellValue() + "\t\t");
+                        break;
+                }
+            }
+            System.out.println();
+        }
+        fIP.close();
     }
 }
